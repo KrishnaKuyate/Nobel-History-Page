cb42e7d0510d0ef6003cccefb69a252a9ee22651
Krishna Kuyate <44673946+KrishnaKuyate@users.noreply.github.com>
2018-11-24 14:15:59 +0530

    Check Dataset Structure

---
+++ Dataset_str.R
@@ -0,0 +1,23 @@
+#N2
+
+####Understand the dataset structure:
+
+##View dataset
+nobel_data
+
+##Datset Variable:
+ncol(nobel_data)
+
+#Variable name:
+colnames(nobel_data)
+
+##Dataset observations:
+nrow(nobel_data)
+
+##Dataset Structure:
+str(nobel_data)
+
+##Summary:
+summary(nobel_data)
+
+
